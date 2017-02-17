271e004256f891531b1eac51c66d36dc5cb6daec
Rukhsar <rukhsar.man@gmail.com>
2017-02-17 13:54:22 +0000

    dependency injected into the TagManager Block

--- Block/TagManager.php
+++ Block/TagManager.php
@@ -9,5 +9,32 @@ use Rukhsar\GoogleTagManager\Helper\Data as GoogleTagManagerHelper;
 
 class TagManager extends Template
 {
+    protected $_googleTagManagerHelper = null;
 
+    protected $_cookieHelper = null;
+
+    protected $_dataLayerModel = null;
+
+    protected $_salesOrderCollection;
+
+    private $_orderCollection;
+
+    protected $_customVariables = array();
+
+    public function __construct(
+        Context $context,
+        GoogleTagManagerHelper $googleTagManagerHelper,
+        CookieHelper $cookieHelper,
+        \Rukhsar\GoogleTagManager\Model\DataLayer $dataLayer,
+        \Magento\Sales\Model\ResourceModel\Order\CollectionFactory $salesOrderCollection,
+        array $data = []
+    )
+    {
+        $this->_cookieHelper = $cookieHelper;
+        $this->_googleTagManagerHelper = $googleTagManagerHelper;
+        $this->_dataLayerModel = $dataLayer;
+        $this->_salesOrderCollection = $salesOrderCollection;
+        $this->_isScopePrivate = true;
+        parent::__construct($context, $data);
+    }
 }
